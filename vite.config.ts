80a4d101e1d829f2c1955bb9961f2ea742a395ef
samkallon <sjlsamkallon@gmail.com>
2025-03-18 17:55:23 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // 路径别名
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src') // 路径别名
+        }
+    },
+    plugins: [
+        vue(),
+        obfuscatorPlugin({
+            options: {
+                optionsPreset: 'high-obfuscation'
+            },
+            apply: 'build'
+        }),
+    ],
+    base: '/three-demo-list/',
+    server: {
+        port: 3000
     }
-  },
-  plugins: [vue()],
-  base:'/three-demo-list/',
-  server:{
-    port:3000
-  }
 })
